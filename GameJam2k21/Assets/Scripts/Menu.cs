4a2831f51e5ec9577f96ffd54f180ebbe9d56c8b
unknown <mort_sel@yahoo.ca>
2021-05-15 21:46:13 -0400

    soundmanager tweak

--- GameJam2k21/Assets/Scripts/Menu.cs
+++ GameJam2k21/Assets/Scripts/Menu.cs
@@ -9,8 +9,6 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AudioSource bgMusic = FindObjectOfType<AudioSource>();
-        DontDestroyOnLoad(bgMusic.gameObject);
         foreach (var btn in FindObjectOfType<Canvas>().GetComponentsInChildren<Button>())
         {
             if (btn.name.Contains("Play"))

4c483ca55d0e14b2b7413e2946294406156ec5c5
unknown <mort_sel@yahoo.ca>
2021-05-16 16:28:08 -0400

    tuto

--- GameJam2k21/Assets/Scripts/Menu.cs
+++ GameJam2k21/Assets/Scripts/Menu.cs
@@ -1,14 +1,34 @@
-
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class Menu : MonoBehaviour
 {
+
+    Image bg;
+    Image tuto;
 
     // Start is called before the first frame update
     void Start()
     {
+        Image[] images = FindObjectsOfType<Image>(true);
+        for (int i = 0; i < images.Length; i++)
+        {
+            Image current = images[i];
+            Debug.Log(current);
+            if (current.name.Contains("Bg"))
+            {
+                bg = current;
+            }
+            if (current.name.Contains("Tuto"))
+            {
+                tuto = current;
+            }
+        }
+
+
         GameObject.FindObjectOfType<AudioManager>().Play("Menu");
         foreach (var btn in FindObjectOfType<Canvas>().GetComponentsInChildren<Button>())
         {
@@ -25,6 +45,14 @@ public class Menu : MonoBehaviour
 
     void Play()
     {
+        StartCoroutine(StartGame());
+    }
+
+    private IEnumerator StartGame()
+    {
+        tuto.gameObject.SetActive(true);
+        bg.gameObject.SetActive(false);
+        yield return new WaitUntil(() => Input.anyKeyDown);
         GameObject.FindObjectOfType<AudioManager>().Stop("Menu");
         SceneManager.LoadScene("Main_Level", LoadSceneMode.Single);
     }
